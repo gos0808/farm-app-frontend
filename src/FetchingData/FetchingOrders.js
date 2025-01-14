8a57b6664797c71cae5611a91bd07052b1873ab7
gos0808 <124739104+gos0808@users.noreply.github.com>
2025-01-13 22:10:42 -0500

    Update FetchingOrders.js

--- src/FetchingData/FetchingOrders.js
+++ src/FetchingData/FetchingOrders.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const saveOrder = ({orderId, quantity, totalPrice, userId }) => {  
-    // axios.post('https://farm-app-backend-1.onrender.com/saveOrder', 
-    axios.post('130.61.33.105:8000/saveOrder', 
+    axios.post('https://farm-app-backend-1.onrender.com/saveOrder', 
+    // axios.post('130.61.33.105:8000/saveOrder', 
                {orderId, quantity, totalPrice, userId })
     .then(() => {
 console.log("Order saved successfully", orderId );
