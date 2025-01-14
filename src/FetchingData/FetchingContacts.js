ea85f039e9de4c973105dac668089b1e6fa0986a
gos0808 <124739104+gos0808@users.noreply.github.com>
2025-01-13 21:03:03 -0500

    Update FetchingContacts.js

--- src/FetchingData/FetchingContacts.js
+++ src/FetchingData/FetchingContacts.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const saveContact = ({ name, email, message, userId }) => {
-    axios.post('https://farm-app-backend-1.onrender.com/saveContact', { name, email, message, userId });
+    // axios.post('https://farm-app-backend-1.onrender.com/saveContact',
+    axios.post('130.61.33.105:8000/saveContact', 
+               { name, email, message, userId });
 };
