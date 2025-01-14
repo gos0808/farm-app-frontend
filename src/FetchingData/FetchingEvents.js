4565d54e4a82db7bd18d7a7bc2c9b272717b31db
gos0808 <124739104+gos0808@users.noreply.github.com>
2025-01-13 21:03:29 -0500

    Update FetchingEvents.js

--- src/FetchingData/FetchingEvents.js
+++ src/FetchingData/FetchingEvents.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const getEvents = (setEvents) => {
-    axios.get('https://farm-app-backend-1.onrender.com/events')
+    // axios.get('https://farm-app-backend-1.onrender.com/events')
+    axios.get('130.61.33.105:8000/events')
         .then(({ data }) => {
             setEvents(data);
         })
