5e248b8cb1bd97eb65b572fc960bb42a9e127dbe
gos0808 <124739104+gos0808@users.noreply.github.com>
2025-01-13 21:04:39 -0500

    Update FetchingProducts.js

--- src/FetchingData/FetchingProducts.js
+++ src/FetchingData/FetchingProducts.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const getProducts = (setProducts) => {
-    axios.get('https://farm-app-backend-1.onrender.com/products')
+    // axios.get('https://farm-app-backend-1.onrender.com/products')
+    axios.get('130.61.33.105:8000/products')
         .then(({ data }) => {
             setProducts(data);
         })
